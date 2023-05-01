ddf9509e739ad2fad3e528991eac456ee30631b1
Pawel Kowalak <pawel@ksoc.com>
2023-05-01 11:56:34 +0200

    Allow to ignore CVEs
    When a list of CVEs to ignore is provided, a Grype config file is generated with ignore entries.
    
    Signed-off-by: Pawel Kowalak <pawel@ksoc.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,12 @@
 
 [ -z ${FAIL_ON_SEVERITY} ] || PARAM_FAIL_ON_SEVERITY="-f ${FAIL_ON_SEVERITY}"
 
-/grype -o template -t /output_with_summary.tmpl ${PARAM_FAIL_ON_SEVERITY} ${IMAGE}
+if [ ! -z "${IGNORE_CVES}" ]; then
+  echo "ignore:" > /config.yaml
+  for cve in ${IGNORE_CVES}; do
+    echo "  - vulnerability: $cve" >> /config.yaml
+  done
+  PARAM_CONFIG="-c /config.yaml"
+fi
+
+/grype -o template -t /output_with_summary.tmpl ${PARAM_CONFIG} ${PARAM_FAIL_ON_SEVERITY} ${IMAGE}
